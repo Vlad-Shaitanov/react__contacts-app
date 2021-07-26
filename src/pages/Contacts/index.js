ab7a27467053b0f3bd42175322f19fdb7ddaed06
Vladislav <kelsenelenelvial@mail.ru>
2021-07-26 15:12:32 +0300

    filtering by fullname

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useContacts } from './useContacts';
 import Container from '@material-ui/core/Container';//Подключение общего контейнера
 import Grid from '@material-ui/core/Grid';//Подключение Grid-сетки
@@ -5,6 +6,7 @@ import Box from "@material-ui/core/Box";
 import { makeStyles, createStyles } from '@material-ui/core/styles';//Стилизация
 import Typography from '@material-ui/core/Typography';//Стилизация заголовков
 import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
 import { ContactsTable } from "./ContactsTable";//Таблица контактов
 import { ToggleDataViewMode } from "./ToggleDataViewMode";
 import { useDataViewMode } from "./useDataViewMode";
@@ -19,14 +21,43 @@ const useStyles = makeStyles((theme) =>
 		headContainer: {
 			marginBottom: theme.spacing(3),// = 8px(default) * 3 = 24px
 		},
+		filtersContainer: {
+
+		},
 	})
 );
 
+//Дефолтные значения фильтров
+const FiltersDafaultValue = {
+	fullname: ""
+};
+
+const filterByFullname = ({ first, last }, fullname) => {
+	return (
+		first?.toLowerCase().includes(fullname.toLowerCase()) ||
+		last?.toLowerCase().includes(fullname.toLowerCase())
+	);
+};
 
 export const Contacts = () => {
 	const classes = useStyles();//Инициализация стилей
 	const contacts = useContacts();
 	const [dataViewMode, setDataViewMode] = useDataViewMode();
+	const [filters, setFilters] = useState(FiltersDafaultValue);
+
+	const handleChangeFilter = (event) => {
+		setFilters((prevFilters) => ({
+			...prevFilters,
+			[event.target.name]: event.target.value,
+		}));
+	};
+
+	//Отфильтрованные контакты
+	const filteredContacts = contacts.data.filter(
+		(c) => filterByFullname(c.name, filters.fullname)
+
+	);
+	console.log(filteredContacts);
 
 	return (
 		<Container className={classes.root}>
@@ -40,6 +71,17 @@ export const Contacts = () => {
 							dataViewMode={dataViewMode}
 							setDataViewMode={setDataViewMode} />
 
+					</Box>
+				</Grid>
+				<Grid item xs={12} className={classes.filtersContainer}>
+					<Box display="flex">
+						<TextField
+							name="fullname"
+							label="Fullname"
+							variant="outlined"
+							size="small"
+							value={filters.fullname}
+							onChange={handleChangeFilter} />
 					</Box>
 				</Grid>
 				<Grid item xs={12}>
@@ -60,7 +102,7 @@ export const Contacts = () => {
 
 						if (dataViewMode === DATA_VIEW_MODES.TABLE) {
 
-							return <ContactsTable data={contacts.data} />
+							return <ContactsTable data={filteredContacts} />
 						}
 
 						if (dataViewMode === DATA_VIEW_MODES.GRID) {
